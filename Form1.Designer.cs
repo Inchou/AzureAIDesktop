9dd231c5df2ed6d595371f91fe4bc54828aa0f9d
Inchou <aaaaa83428@gmail.com>
2022-04-27 15:13:07 +0800

    0427

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace AzureAIDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(32, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 384);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(629, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "辨識";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace AzureAIDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            string key = "74f444b0708b49ea940365ced892eaf3";
+            string endPoint = "https://msit133inchoufaceservice.cognitiveservices.azure.com";
+            string imgUrl = "https://static.leiphone.com/uploads/new/images/20191020/5dac1e9e621fa.jpg?imageView2/2/w/740";
+            HttpClient client = new HttpClient();
+            using (HttpResponseMessage imgResponse=await client.GetAsync(imgUrl))
+            {
+                imgResponse.EnsureSuccessStatusCode();
+                using(Stream imgStream = await imgResponse.Content.ReadAsStreamAsync())
+                {
+                    pictureBox1.Image = Image.FromStream(imgStream);
+                }
+            }
+            NameValueCollection queryString = HttpUtility.ParseQueryString(string.Empty);
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-key", key);
+            queryString["returnFaceId"] = "true";
+            queryString["returnFaceLandmarks"] = "false";
+            queryString["returnFaceAttributes"] = "age,gender,emotion";
+            queryString["recognitionModel"] = "recognition_01";
+            queryString["returnRecognitionModel"] = "false";
+            queryString["detectionModel"] = "detection_01";
+            string uri = $"{endPoint}/face/v1.0/detect?{queryString}";
+
+            JObject data = new JObject { ["url"] = imgUrl };
+            string json = JsonConvert.SerializeObject(data);
+            StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(uri, stringContent);
+            string content = await response.Content.ReadAsStringAsync();
+            //MessageBox.Show(content);
+            dynamic faces = JsonConvert.DeserializeObject(content);
+            foreach(var item in faces)
+            {
+                JObject face = item as JObject;
+                int age = Convert.ToInt32(face["faceAttributes"]["age"]);
+                string gender = Convert.ToString(face["faceAttributes"]["gender"]);
+                MessageBox.Show($"age:{age},gender:{gender}");
+            }
+        }
+    }
+}
